1405df1fcdfd0783757121c66db633c39095bdca
Lucius Fowler <luciusfow@gmail.com>
2020-12-20 12:38:01 -0500

    commit tree 2fe43d614f17423c8fcfd11c062474d200d767a5

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,37 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash[:winter].each do |hol,sup|
+  sup << (supply)
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << (supply)
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
-  # remember to return the updated hash
-
+  # remember to return the updated hash #can't wrap my head around this
+holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+holiday_hash[:winter].map do |hol, sup|
+sup
+end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +59,26 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  #needed help with this one 
+  holiday_hash.each do |sea, hol|
+    puts "#{sea.capitalize}:"    
+    hol.each do |hol, sup|
+      puts "  #{hol.to_s.split("_").map{|hol_arr| hol_arr.capitalize}.join(" ")}: #{sup.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+array1 = holiday_hash.map do |season, holidays|
+  holidays.map do |holiday, supplies|
+    if supplies.include?("BBQ")
+      holiday
+    end
+    end
+  end
+  array1.flatten.compact #.compact returns array with all nil elements removed
 end
 
 
